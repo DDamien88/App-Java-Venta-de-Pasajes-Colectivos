8f1a4f19f7ab62dd8fb3ca0bfa8654d5fe9373ed
DamiánD <deramodamian@gmail.com>
2024-05-28 17:49:35 -0300

    prueba

--- VentaDePasajesGrupo17/src/ventadepasajesgrupo17/VentaDePasajesGrupo17.java
+++ VentaDePasajesGrupo17/src/ventadepasajesgrupo17/VentaDePasajesGrupo17.java
@@ -16,7 +16,7 @@ public class VentaDePasajesGrupo17 {
     public static void main(String[] args) {
         // TODO code application logic here
         //Damián
-        //asdadsa
+        //asda
     }
     
 }
