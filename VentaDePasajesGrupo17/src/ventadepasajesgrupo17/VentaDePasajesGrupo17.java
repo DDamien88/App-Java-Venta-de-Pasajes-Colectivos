51d69d6ed735cdf78c38671b07c70e6823ba0d74
DamiánD <deramodamian@gmail.com>
2024-05-31 17:07:55 -0300

    Revert "Merge branch 'Damián'"
    This reverts commit 704dfce55e4cc67277e38113d77e98a42c82f6f5, reversing
    changes made to db6d157020ac244dd2c6693760c69a11b656234d.

--- VentaDePasajesGrupo17/src/ventadepasajesgrupo17/VentaDePasajesGrupo17.java
+++ VentaDePasajesGrupo17/src/ventadepasajesgrupo17/VentaDePasajesGrupo17.java
@@ -4,8 +4,13 @@
  */
 package ventadepasajesgrupo17;
 
-import ventadepasajesgrupo17.accesoADatos.ColectivoData;
-import ventadepasajesgrupo17.entidades.Colectivo;
+import java.time.LocalTime;
+import ventadepasajesgrupo17.accesoADatos.HorarioData;
+import ventadepasajesgrupo17.accesoADatos.PasajeroData;
+import ventadepasajesgrupo17.accesoADatos.RutaData;
+import ventadepasajesgrupo17.entidades.Horario;
+import ventadepasajesgrupo17.entidades.Pasajero;
+import ventadepasajesgrupo17.entidades.Ruta;
 
 /**
  *
@@ -17,29 +22,60 @@ public class VentaDePasajesGrupo17 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        ColectivoData coleData= new ColectivoData();
-        //Colectivo cole= new Colectivo("pha-589", "VW", "2018", 30);
-        
-        //coleData.guardarColectivo(cole);
-        //coleData.modificarColectivo(cole);
-        //coleData.eliminarColectivo(2);
-          /*Colectivo buscarCole= coleData.buscarColectivo(1);
-          if (buscarCole != null) {
-              System.out.println("ID: " + buscarCole.getId_colectivo());
-              System.out.println("Matricula: " + buscarCole.getMatricula());
-              System.out.println("Marca: " +buscarCole.getMarca());
-              System.out.println("Capacidad: " +buscarCole.getCapacidad());
+
+        /*Pasajero pasa = new Pasajero(1, "Jose Carlos", "Sosa", "49829384", "joseCarlos@gmail.com", "2554628765");
+        Pasajero pasa2 = new Pasajero("Mario", "Gomez", "42357986", "marito@gmail.com", "2664975836");
+        PasajeroData p1 = new PasajeroData();*/
+//       p1.guardarPasajero(pasa2);
+//       p1.actualizarPasajero(pasa);
+//       p1.eliminarPasajero(3);
+//        Pasajero pasajEncontrado = p1.buscarPasajeroPorDNI("49829384");
+//        if(pasajEncontrado != null){
+//            System.out.println("dni: " + pasajEncontrado.getDni());
+//            System.out.println("Nombre: " + pasajEncontrado.getNombre());
+//        }
+//        for(Pasajero pasajero : p1.listarPasajeros()){
+//            System.out.println(pasajero.getNombre());
+//            System.out.println(pasajero.getApellido());
+//            System.out.println(pasajero.getDni());
+//            System.out.println(pasajero.getCorreo());
+//            System.out.println(pasajero.getTelefono());
+//        }
+//        for (Pasajero pasajero : p1.buscarPasajeroPorNombre("Jose")) {
+//            System.out.println(pasajero.getNombre());
+//            System.out.println(pasajero.getApellido());
+//            System.out.println(pasajero.getDni());
+//            System.out.println(pasajero.getCorreo());
+//            System.out.println(pasajero.getTelefono());
+//        }
+        Ruta ruta = new Ruta("Tilisarao", "Villa Mercedes", LocalTime.of(22, 0));
+        Ruta ruta2 = new Ruta(3,"Naschel","Concaran",LocalTime.of(15, 0));
+        RutaData ru = new RutaData();
+            
+//        ru.guardarRuta(ruta2);
+        /*for (Ruta rut : ru.listarRuta()) {
+            System.out.println(rut.getDestino());
         }*/
-          
-          for (Colectivo colec : coleData.listarColes()) {
-              System.out.println("Marca: " + colec.getMarca());
-              System.out.println("Matrícula: " + colec.getMatricula());
-              System.out.println("Capacidad: " + colec.getCapacidad());
-        }
-        
-        
-       
+//        ru.actualizarRuta(ruta);
+//        ru.actualizarRuta(ruta2);
+        ru.eliminarRuta(2);
+      
+
+//        for (Ruta rut : ru.buscarOrigen("La Quiaca")) {
+//            System.out.println(rut);
+//        }
+        /*for (Ruta rut : ru.buscarDestino("Villa Mercedes")) {
+            System.out.println(rut);
+        }*/
+
+//        Ruta rut = ru.buscarRuta(1);
+//        //Horario hora = new Horario(ruta, LocalTime.of(22, 15), LocalTime.of(00, 15));
+//        HorarioData hr = new HorarioData();
+//        //hr.guardarHorario(hora, rut);*/
+//        
+//        for (Horario hrs : hr.buscarHorariosPorRuta(rut)) {
+//            System.out.println("horarios por ruta: " + rut.getId_ruta() + "; " + hrs);
+//        }
     }
-    
+
 }
